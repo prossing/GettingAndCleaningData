b2d99eda56c324b5629ecd667db2a56f8f6c42b3
prossing <pontus@rossing.se>
2015-06-22 03:22:58 +0200

    Modified run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,15 @@
 ## Getting and Cleaning Data Course Project
 
+library(data.table)
+library(reshape2)
+
 ## Asking the user if data download is required.
 
 userInput <- tolower(readline("Do you want to download and unzip the UCI HAR Dataset to your current working directory? Y/N: "))
 
-if (userInput == 'y') {
+if(userInput == 'y') {
     getData()
-} else if (file.exists("./UCI HAR Dataset")) {
+} else if(file.exists("./UCI HAR Dataset")) {
     print('Data is in working directory')
 } else {
     print("Data not found in working directory") 
@@ -23,37 +26,58 @@ getData <- function() {
 
 ## Reading data into R as data.table than merge test and train
 
-library(data.table)
-library(reshape2)
 
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[, 2]
+
+## Gathering test files
 test_X <- read.table("./UCI HAR Dataset/test/X_test.txt")
-names(test_X) <- read.table("./UCI HAR Dataset/features.txt")[, 2]
+names(test_X) = read.table("./UCI HAR Dataset/features.txt")[, 2]
 test_Y <- read.table("./UCI HAR Dataset/test/Y_test.txt")
 test_subject <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-test_compl <- cbind(test_subject, test_Y, test_X)
+test_subject <- as.data.table(test_subject)
 
+## Extracting mean and standard deviation data
+test_X = test_X[, grep("mean|std", names(test_X))]
+
+## Adding names to test data
+test_Y[, 2] = activity_labels[test_Y[, 1]]
+names(test_Y) = c("ActivityID", "Activity")
+setnames(test_subject, "V1", "Subject")
+
+## Merging test files
+test_compl = cbind(test_subject, test_Y, test_X)
+
+## Gathering train files
 train_X <- read.table("./UCI HAR Dataset/train/X_train.txt")
-names(train_X) <- read.table("./UCI HAR Dataset/features.txt")[,2]
+names(train_X) = read.table("./UCI HAR Dataset/features.txt")[, 2]
 train_Y <- read.table("./UCI HAR Dataset/train/Y_train.txt")
 train_subject <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-train_compl <- cbind(train_subject, train_Y, train_X)
+train_subject <- as.data.table(train_subject)
 
+## Extracting mean and standard deviation data
+train_X = train_X[, grep("mean|std", names(train_X))]
+
+## Adding names to train data
+train_Y[, 2] = activity_labels[train_Y[, 1]]
+names(train_Y) = c("ActivityID", "Activity")
+setnames(train_subject, "V1", "Subject")
+
+## Merging train files
+train_compl = cbind(train_subject, train_Y, train_X)
 
 feautures <- read.table("./UCI HAR Dataset/features.txt") ##REDUNDANT
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+    
+## Merging test and train
+test_train = rbind(test_compl, train_compl)
 
+## Extracting the mean and standard deviation for each measurement
 
+test_train_mean_stdev = test_train[, grep("mean|std", names(test_train))]
 
-## (1) Merging of the training set and test set
+id_var <- c("Subject", "ActivityID", "Activity")
+meas_var <- setdiff(colnames(test_train_mean_stdev), id_var)
 
-test_train <- rbind(test_compl, train_compl)
+melt_data = melt(test_train_mean_stdev, id = id_var, measure.vars = meas_var)
+final_data <- dcast(melt_data, Subject + Activity ~ variable, mean)
+write.table(final_data, file = "./UCI_HAR_tidy.txt", row.name = FALSE)
 
-## (2) Extract the mean and standard deviation for each measurement
-
-test_train_mean_stdev <- test_train[, grep("mean|std", names(test_train))]
-
-## (3) Use descriptive activity names
-
-## (4) Descriptive variable names
-
-## (5) A second independent data set with the average of each variable for each activity and each subject
